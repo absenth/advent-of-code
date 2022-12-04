c987249a2527e57a979092b7b9be14d1a272408e
absenth <1115138+absenth@users.noreply.github.com>
2022-12-04 11:14:45 -0600

    Day 4 Part2 completed

--- 2022-Advent_of_code/2022-12-04/challenge4.py
+++ 2022-Advent_of_code/2022-12-04/challenge4.py
@@ -1,13 +1,16 @@
 def main():
-  range_overlaps = 0
+  pairs_overlaps, range_overlaps = 0, 0
   with open('input4.txt') as assignment_ranges:
     for assignment_range in assignment_ranges:
       ar1, ar2 = assignment_range.split(',')
       ar1s, ar1e, ar2s, ar2e = range_builder(ar1, ar2)
-      overlaps = overlap_checking(range(ar1s, ar1e), range(ar2s, ar2e))
+      overlaps = overlap_checking(ar1s, ar1e, ar2s, ar2e)
       if overlaps:
         range_overlaps += 1
-    print(range_overlaps)
+      pairs = pairs_checking(ar1s, ar1e, ar2s, ar2e)
+      if pairs:
+          pairs_overlaps += 1
+    print(f' Part1: {range_overlaps},  Part2: {pairs_overlaps}')
 
 
 def range_builder(ar1, ar2):
@@ -20,20 +23,17 @@ def range_builder(ar1, ar2):
   return ar1_start, ar1_end, ar2_start, ar2_end
 
 
-def overlap_checking(assignment_range1, assignment_range2):
-  if not assignment_range1:
-    return True
-  if not assignment_range2:
-    return False
-  if len(assignment_range1) > 1 and assignment_range1.step % assignment_range2.step:
-    return False
-  if len(assignment_range2) > 1 and assignment_range2.step % assignment_range1.step:
-    return False
-  if assignment_range1.start in assignment_range2 and assignment_range1[-1] in assignment_range2:
-    return True
-  if assignment_range2.start in assignment_range1 and assignment_range2[-1] in assignment_range1:
-    return True
+def overlap_checking(ar1s, ar1e, ar2s, ar2e):
+    if set((range(ar1s, ar1e))).issubset(range(ar2s, ar2e)):
+        return True
+    if set((range(ar2s, ar2e))).issubset(range(ar1s, ar1e)):
+        return True
 
+
+def pairs_checking(ar1s, ar1e, ar2s, ar2e):
+    for i in range(ar1s, ar1e):
+        if i in range(ar2s, ar2e):
+            return True
 
 if __name__ == '__main__':
   main()
